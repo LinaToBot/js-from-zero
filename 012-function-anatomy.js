4e341f79bafd7e187575dae6f5928a065acbd730
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-05-14 19:23:23 -0600

    Function anatomy

---
+++ 012-function-anatomy.js
@@ -0,0 +1,31 @@
+// FUNCTION ANATOMY
+
+function sum(a, b) {
+  return a + b;
+}
+
+// key word         --> function
+// name             --> sum
+// Parameters       --> a , b
+// Curly brackets   --> {}
+// Body (return)    --> return a + b
+
+sum(3, 5);
+// Called           --> name of the function and parenthesis: sum()
+// Arguments        --> values in a function called
+
+// Price with a discount
+function calculateDiscountedPrice(price, discountPercentage) {
+  const discount = (price * discountPercentage) / 100;
+  const priceDiscount = price - discount;
+
+  return priceDiscount;
+}
+
+const originalPrice = 55000;
+const discountPercentage = 20;
+const finalPrice = calculateDiscountedPrice(originalPrice, discountPercentage);
+
+console.log("Original price: $" + originalPrice);
+console.log("Discount: " + discountPercentage + "%");
+console.log("Price with discount: $" + finalPrice);
