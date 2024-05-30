e0d8124eadcb6f064c4ed967250a3bc7b88def80
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-05-29 18:23:46 -0600

    conditional execution: if

---
+++ 03-logic-and-cicles/028-condicional-execution-if.js
@@ -0,0 +1,11 @@
+// control structure if, it select part of the code to execute.
+
+let cats = "5";
+
+if (cats === "5") {
+  console.log("There's 5 cats");
+} else if (cats === 3) {
+  console.log("There's 3 cats");
+} else {
+  console.log("I don't know who many cats are there");
+}
