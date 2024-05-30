538fa8c688bf4fa43bbfff4ceea248817428b9e9
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-05-29 21:13:08 -0600

    Loop: for each

---
+++ 03-logic-and-cicles/033-loop-for-each.js
@@ -0,0 +1,14 @@
+/* 
+LOOP: FOR EACH
+
+array.forEach((item) => {
+ code to run
+})
+
+*/
+
+let list = ["20", "76", "65", "17"];
+
+list.forEach((item) => {
+  console.log((item += " is the cost of the product"));
+});
